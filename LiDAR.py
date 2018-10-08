31d488d06e60877cfd8e4deba523aca4622d566e
babusid <sidharth.n.babu@gmail.com>
2018-10-08 14:11:37 -0400

    Functioning average distance function

--- LiDAR.py
+++ LiDAR.py
@@ -1,7 +1,25 @@
 #!/usr/bin/env python
 import rospy
+import numpy as np
+from sensor_msgs.msg import LaserScan
 
-pub = rospy.SubscribeListener('/scan')
 
-while True:
-    print(pub)
+def average_distance(data, a=0, b=270):
+    PPD = 4 # points per degree
+    data = data[a*PPD:b*PPD]
+    arr = np.array(data)
+    return arr.mean()
+
+
+def print_data(data):
+    print(average_distance(data.ranges, 120, 150))
+    #print(len(data.ranges))
+    #print(data.intensities)
+
+
+rospy.init_node('hello_darkness')
+pub = rospy.Subscriber('scan', LaserScan, print_data, queue_size=2)
+
+
+while not rospy.is_shutdown():
+    pass
