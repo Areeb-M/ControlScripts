5e2618a38338d231375e01122f9050f34aa9ed80
Areeb Mohammed <areebmohammed11@gmail.com>
2018-10-16 13:47:53 -0400

    Further implement DistanceInterrupt controller
    Signed-off-by: Areeb Mohammed <areebmohammed11@gmail.com>

--- Controllers/DistanceInterrupt.py
+++ Controllers/DistanceInterrupt.py
@@ -1,4 +1,5 @@
 from Controller import Controller
+import numpy as np
 
 
 class DistanceInterrupt(Controller):
@@ -8,4 +9,11 @@ class DistanceInterrupt(Controller):
     def update(self, data):
         pass
 
-    def
+    def HALT(self):
+        pass
+
+def average_distance(data, a=0, b=270):
+    PPD = 4 # points per degree
+    data = data[a*PPD:b*PPD]
+    arr = np.array(data)
+    return arr.mean()
